a1cefebbfc99db6407d56e2a924e07ba9671b523
XIANJU ZENG <z1169122954@gmail.com>
2026-09-04 17:28:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -34,9 +34,11 @@ function drinkInvite() {
                 font-family:sans-serif;
             ">
                 <h2>🍻 酒局邀请</h2>
-                <h2> </h2>
-                <h2> </h2>
-                <h2> </h2>
+                </br>
+                </br>
+                </br>
+                </br>
+                </br>
                 <p>Dear Li,今晚一起喝一杯？</p>
 
                 <div style="display:flex;gap:15px;justify-content:center;">
